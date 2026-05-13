63df816c9fee73703b5db754a5c7f0a0641442d0
Arnz3 <cuyversarno@gmail.com>
2026-05-13 17:21:09 +0200

    change host

--- main.py
+++ main.py
@@ -87,4 +87,4 @@ async def print_ticket(order: OrderData):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", port=8000, reload=True)
